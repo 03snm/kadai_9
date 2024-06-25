f00e7712b450ca4466c5e7bfa5fa4182f9721adf
03snm <iwata.peta@gmail.com>
2024-06-25 18:36:25 +0900

    update repeat -> 10 times kadai_9.py

--- kadai_9.py
+++ kadai_9.py
@@ -1,15 +1,18 @@
 import random
 answer = random.randint(100, 999)
+i = 0
 
-while True:
+while i < 10:
     number = int(input('100~999の間の整数を入力してください：'))
 
     if answer == number:
         print(f'正解です! 答えは{answer}')
         break
-    
+
     elif answer > number:
         print(f'不正解です。答えは、入力された値{number}よりも大きいです。')
 
     else:
         print(f'不正解です。答えは、入力された値{number}よりも小さいです。')
+
+    i+=1
