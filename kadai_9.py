3487281f44834fb50599bf2ac4ce5f16ad06ced7
03snm <iwata.peta@gmail.com>
2024-06-25 18:31:00 +0900

    add correct or incorrect feedback kadai_9.py

--- kadai_9.py
+++ kadai_9.py
@@ -1,4 +1,12 @@
 import random
 answer = random.randint(100, 999)
-print(answer)
-number = int(input('100~999の間の整数を入力してください：'))
+
+while True:
+    number = int(input('100~999の間の整数を入力してください：'))
+
+    if answer == number:
+        print(f'正解です! 答えは{answer}')
+        break
+
+    else:
+        print(f'不正解です。入力された値は{number}です。')
